751e9102e5ce2db67a9ceddd3bc0db8cb4fae436
nviel <le.nicolas.viel@gmail.com>
2018-09-09 22:28:06 +0200

    suppression de 'fierement propulsé par...'

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -15,7 +15,4 @@
 		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
 	}
 	?>
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" class="imprint">
-		<?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'Nicolas' ); ?>
-	</a>
 </div><!-- .site-info -->
